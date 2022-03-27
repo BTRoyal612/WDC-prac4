1669283bd899fbdfc7dfba514b35dc7175ab743e
Gia Bao Hoang <83662919+BTRoyal612@users.noreply.github.com>
2022-03-27 12:32:42 +0000

    task 3.2

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,6 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-// Task 3.1
 var timestamp;
 router.get('/last.txt', function(req, res, next) {
   if (timestamp) res.send(String(timestamp));
@@ -15,41 +14,43 @@ router.get('/last.txt', function(req, res, next) {
   timestamp = currentDate.getTime();
 });
 
-// Task 3.2
-// var counter = 0;
-// router.get('/color.html', function(req, res, next) {
-//   var color;
-//   switch (counter%4) {
-//     case 1:
-//       color = "yellow";
-//       break;
-//     case 2:
-//       color = "green";
-//       break;
-//     case 3:
-//       color = "blue";
-//       break;
-//     default:
-//       color = "red";
-//   }
-//   counter++;
+var counter = 0;
+router.get('/color.html', function(req, res, next) {
+  var color = "";
+  switch (counter%4) {
+    case 1:
+      color = "yellow";
+      break;
 
-//   res.send(`
-//   <!DOCTYPE html>
-//     <html lang="en">
-//     <style>
-//       h1 {
-//         color: ${color};
-//       }
-//     </style>
-//     <head>
-//         <title>Task 3.2</title>
-//     </head>
-//     <body>
-//         <h1>${color}</h1>
-//     </body>
-//     </html>
-//   `);
-// });
+    case 2:
+      color = "green";
+      break;
+
+    case 3:
+      color = "blue";
+      break;
+
+    default:
+      color = "red";
+  }
+  counter++;
+
+  res.send(`
+  <!DOCTYPE html>
+  <html lang="en">
+      <head>
+          <title>Task 3.2</title>
+          <style>
+            h1 {
+              color: ${color};
+            }
+          </style>
+      </head>
+      <body>
+          <h1>${color}</h1>
+      </body>
+  </html>`);
+
+});
 
 module.exports = router;
