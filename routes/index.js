0a7ce5c18e708ced95430cdf1c1551d7877dec09
Gia Bao Hoang <83662919+BTRoyal612@users.noreply.github.com>
2022-03-27 10:50:04 +0000

    task 3.1

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,11 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var timestamp;
+var currentDate = new Date();
+router.get('/last.txt', function(req, res, next) {
+  res.send(String(timestamp));
+  timestamp = currentDate.getTime();
+});
+
 module.exports = router;
