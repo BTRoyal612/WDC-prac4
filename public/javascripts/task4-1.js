4cd9608439c089da55993eb1d7e3da418cfa6d3b
Gia Bao Hoang <83662919+BTRoyal612@users.noreply.github.com>
2022-03-27 14:56:12 +0000

    task 4.1

---
+++ public/javascripts/task4-1.js
@@ -0,0 +1,13 @@
+function getTimestamp() {
+    var xhttp = new XMLHttpRequest();
+
+    xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("time-stamp").innerText = "This page was last view by " + this.responseText;
+        }
+    }
+
+    xhttp.open("GET", "/last.txt", true)
+
+    xhttp.send();
+}
