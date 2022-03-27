23f1c955f5cf65f2d61ff3602adf4d64b8454ca6
Gia Bao Hoang <83662919+BTRoyal612@users.noreply.github.com>
2022-03-27 11:21:32 +0000

    task 3.2

--- routes/index.js
+++ routes/index.js
@@ -16,40 +16,40 @@ router.get('/last.txt', function(req, res, next) {
 });
 
 // Task 3.2
-var counter = 0;
-router.get('/color.html', function(req, res, next) {
-  var color;
-  switch (counter%4) {
-    case 1:
-      color = "yellow";
-      break;
-    case 2:
-      color = "green";
-      break;
-    case 3:
-      color = "blue";
-      break;
-    default:
-      color = "red";
-  }
-  counter++;
+// var counter = 0;
+// router.get('/color.html', function(req, res, next) {
+//   var color;
+//   switch (counter%4) {
+//     case 1:
+//       color = "yellow";
+//       break;
+//     case 2:
+//       color = "green";
+//       break;
+//     case 3:
+//       color = "blue";
+//       break;
+//     default:
+//       color = "red";
+//   }
+//   counter++;
 
-  res.send(`
-  <!DOCTYPE html>
-    <html lang="en">
-    <style>
-      h1 {
-        color: ${color};
-      }
-    </style>
-    <head>
-        <title>Task 3.2</title>
-    </head>
-    <body>
-        <h1>${color}</h1>
-    </body>
-    </html>
-  `);
-});
+//   res.send(`
+//   <!DOCTYPE html>
+//     <html lang="en">
+//     <style>
+//       h1 {
+//         color: ${color};
+//       }
+//     </style>
+//     <head>
+//         <title>Task 3.2</title>
+//     </head>
+//     <body>
+//         <h1>${color}</h1>
+//     </body>
+//     </html>
+//   `);
+// });
 
 module.exports = router;
