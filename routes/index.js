c4287c2e4d2c36f0a49caf8ad7896d9cf3418e38
Gia Bao Hoang <83662919+BTRoyal612@users.noreply.github.com>
2022-03-27 12:36:28 +0000

    check task 3.1

--- routes/index.js
+++ routes/index.js
@@ -14,43 +14,4 @@ router.get('/last.txt', function(req, res, next) {
   timestamp = currentDate.getTime();
 });
 
-var counter = 0;
-router.get('/color.html', function(req, res, next) {
-  var color = "";
-  switch (counter%4) {
-    case 1:
-      color = "yellow";
-      break;
-
-    case 2:
-      color = "green";
-      break;
-
-    case 3:
-      color = "blue";
-      break;
-
-    default:
-      color = "red";
-  }
-  counter++;
-
-  res.send(`
-  <!DOCTYPE html>
-  <html lang="en">
-      <head>
-          <title>Task 3.2</title>
-          <style>
-            h1 {
-              color: ${color};
-            }
-          </style>
-      </head>
-      <body>
-          <h1>${color}</h1>
-      </body>
-  </html>`);
-
-});
-
 module.exports = router;
